638ff0812b4509c70b80e679be71a0fc6658c16d
luolulu <luo@LUOdeMBP.hsd1.tx.comcast.net>
2018-06-29 16:10:04 -0500

    the first version

---
+++ src/main/java/onlineShop/service/SalesOrderService.java
@@ -0,0 +1,10 @@
+
+package onlineShop.service;
+
+import onlineShop.model.SalesOrder;
+
+public interface SalesOrderService {
+	void addSalesOrder(SalesOrder salesOrder);
+
+	double getSalesOrderTotal(int cartId);
+}
